b01ad69bdb1c80d502a4bba0a0b3d23c5985ea42
Dmitry Petrov <61933931+dmitrypetrov-dev@users.noreply.github.com>
2020-06-18 00:31:08 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 //скролл вниз по нажатию на стрелку
 
-(function() {
+(function () {
     'use strict';
     var btnScrollDown = document.querySelector('.intro__arrow');
+
     function scrollDown() {
         var windowCoords = 1270;
-        console.log(windowCoords);
         (function scroll() {
             if (window.pageYOffset < windowCoords) {
                 window.scrollBy(0, 10);
@@ -13,5 +13,21 @@
             }
         })();
     }
+
     btnScrollDown.addEventListener('click', scrollDown);
 })();
+
+// jquery слайдер
+
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        fade: true,
+        dotsClass: 'slider-buttons',
+        autoplay: 1500,
+        speed: 1000,
+        cssEase: 'linear',
+    });
+});
