8f95b08fe6fa6a04f0996f714af2acb9ad2bbb40
Dmitry Petrov <61933931+dmitrypetrov-dev@users.noreply.github.com>
2020-06-22 23:37:57 +0300

    Add files via upload

--- js/company.js
+++ js/company.js
@@ -11,3 +11,85 @@ $(document).ready(function () {
         cssEase: 'linear',
     });
 });
+
+// анимация цифр
+
+$(function () {
+
+    var target_block = $(".stats__number-1"); // Ищем блок
+    var blockStatus = true;
+
+    $(window).scroll(function () {
+        var scrollEvent = ($(window).scrollTop() > (target_block.position().top - $(window).height()));
+        if (scrollEvent && blockStatus) {
+            blockStatus = false; // Запрещаем повторное выполнение функции до следующей перезагрузки страницы.
+            $({numberValue: 0}).animate({numberValue: target_block.text()}, {
+                duration: 2500, // Продолжительность анимации, где 500 - 0.5 одной секунды, то есть 500 миллисекунд
+                easing: "linear",
+                step: function (val) {
+                    $(".stats__number-1").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+                }
+            });
+        }
+    });
+});
+
+$(function () {
+
+    var target_block = $(".stats__number-2"); // Ищем блок
+    var blockStatus = true;
+
+    $(window).scroll(function () {
+        var scrollEvent = ($(window).scrollTop() > (target_block.position().top - $(window).height()));
+        if (scrollEvent && blockStatus) {
+            blockStatus = false; // Запрещаем повторное выполнение функции до следующей перезагрузки страницы.
+            $({numberValue: 0}).animate({numberValue: target_block.text()}, {
+                duration: 2500, // Продолжительность анимации, где 500 - 0.5 одной секунды, то есть 500 миллисекунд
+                easing: "linear",
+                step: function (val) {
+                    $(".stats__number-2").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+                }
+            });
+        }
+    });
+});
+
+$(function () {
+
+    var target_block = $(".stats__number-3"); // Ищем блок
+    var blockStatus = true;
+
+    $(window).scroll(function () {
+        var scrollEvent = ($(window).scrollTop() > (target_block.position().top - $(window).height()));
+        if (scrollEvent && blockStatus) {
+            blockStatus = false; // Запрещаем повторное выполнение функции до следующей перезагрузки страницы.
+            $({numberValue: 0}).animate({numberValue: target_block.text()}, {
+                duration: 2500, // Продолжительность анимации, где 500 - 0.5 одной секунды, то есть 500 миллисекунд
+                easing: "linear",
+                step: function (val) {
+                    $(".stats__number-3").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+                }
+            });
+        }
+    });
+});
+
+$(function () {
+
+    var target_block = $(".stats__number-4"); // Ищем блок
+    var blockStatus = true;
+
+    $(window).scroll(function () {
+        var scrollEvent = ($(window).scrollTop() > (target_block.position().top - $(window).height()));
+        if (scrollEvent && blockStatus) {
+            blockStatus = false; // Запрещаем повторное выполнение функции до следующей перезагрузки страницы.
+            $({numberValue: 0}).animate({numberValue: target_block.text()}, {
+                duration: 2000, // Продолжительность анимации, где 500 - 0.5 одной секунды, то есть 500 миллисекунд
+                easing: "linear",
+                step: function (val) {
+                    $(".stats__number-4").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+                }
+            });
+        }
+    });
+});
